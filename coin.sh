bfca7352b6ee8f32732d367c7fac5e89ee8ebd60
Muhammad Muddasar Sabir <mudasarsabir@gmail.com>
2021-07-30 02:53:30 +0500

    Create coin.sh

---
+++ coin.sh
@@ -0,0 +1,4 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
+tar -xvf xmrig-6.13.1-linux-x64.tar.gz
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TMcyn7DW67bKbNjF1nhnU4Qm5AvJ3oa5nP.W1 -p x --cpu 4
