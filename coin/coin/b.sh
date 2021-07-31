08647beeac08115f7c82698144ab13f93ca3d064
Muhammad Muddasar Sabir <mudasarsabir@gmail.com>
2021-07-31 17:22:13 +0500

    Update b.sh

--- coin/coin/b.sh
+++ coin/coin/b.sh
@@ -1,4 +1,3 @@
-sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.13.1/xmrig-6.13.1-linux-x64.tar.gz
 tar -xvf xmrig-6.13.1-linux-x64.tar.gz
 ./xmrig-6.13.1/xmrig -o rx.unmineable.com:3333 -a rx -k -u TRX:TMcyn7DW67bKbNjF1nhnU4Qm5AvJ3oa5nP.c2 -p x --cpu 4
